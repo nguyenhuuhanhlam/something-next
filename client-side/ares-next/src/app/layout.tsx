2abace86967aa80f5f1074c417763994dadb1ad9
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2023-07-12 22:53:34 +0700

    ...

--- client-side/ares-next/src/app/layout.tsx
+++ client-side/ares-next/src/app/layout.tsx
@@ -2,16 +2,16 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Barlow } from 'next/font/google'
 
-const barlow = Barlow({ subsets: ['latin'] })
+const barlow = Barlow({ subsets: ['latin'], weight: '200' })
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={barlow.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={barlow.className}>{children}</body>
+		</html>
+	)
 }
