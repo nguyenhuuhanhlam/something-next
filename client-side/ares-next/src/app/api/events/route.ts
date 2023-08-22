c7a2d657d2d96fb61ded9c1d8c6c940a1d429e00
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2023-08-22 23:43:46 +0700

    ...

--- client-side/ares-next/src/app/api/events/route.ts
+++ client-side/ares-next/src/app/api/events/route.ts
@@ -14,6 +14,21 @@ export async function POST (req) {
 	const data = await req.formData()
 	let body = Object.fromEntries(data)
 	console.log('BITRIX24::EVENTS', body)
+	
+	/* output:
+
+		{
+		  event: 'ONCRMDYNAMICITEMUPDATE',
+		  'data[FIELDS][ID]': '351',
+		  'data[FIELDS][ENTITY_TYPE_ID]': '132',
+		  ts: '1692722035',
+		  'auth[domain]': 'aresen.bitrix24.com',
+		  'auth[client_endpoint]': 'https://aresen.bitrix24.com/rest/',
+		  'auth[server_endpoint]': 'https://oauth.bitrix.info/rest/',
+		  'auth[member_id]': '8559bb70602901c27836e1ca8138f882',
+		  'auth[application_token]': '00a6r54fbip89zhv0delumtzwkeoo80g'
+		}
+	*/
 
 	// - - - - - - - - - -
 	//socket.emit('type-of-event', 'data of event')
