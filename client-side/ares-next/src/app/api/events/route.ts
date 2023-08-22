56505317a39c0341503d0a050092953bfa070a1a
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2023-08-22 23:24:29 +0700

    ...

--- client-side/ares-next/src/app/api/events/route.ts
+++ client-side/ares-next/src/app/api/events/route.ts
@@ -3,18 +3,19 @@ import { io } from 'socket.io-client'
 
 export async function POST (req) {
 	
-	const socket = io('ws://localhost:3033')
+	//const socket = io('ws://localhost:3033')
 
 	// - - - - - - - - - -
 	/* PA1 */
-	const data = req.nextUrl.searchParams.get('hello')
+	const data = req.nextUrl
+	console.log(data)
 
 	/* PA2 */
 	// const data = await req.formData()
 	// let body = Object.fromEntries(data)
 
 	// - - - - - - - - - -
-	socket.emit('type-of-event', 'data of event')
+	//socket.emit('type-of-event', 'data of event')
 
 
 	return NextResponse.json({ events:true })
