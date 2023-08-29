c93feca7d7ee600d378ccf42d0b65ba150b89ba1
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2023-08-29 21:11:13 +0700

    ...

--- client-side/ares-next/src/app/api/events/route.ts
+++ client-side/ares-next/src/app/api/events/route.ts
@@ -1,42 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server'
-// import { io } from 'socket.io-client'
-
 import { addSPA, updateSPA, deleteSPA } from './spa.funcs.ts'
 
 export async function POST (req) {
 
-	//const socket = io('ws://localhost:3033')
-
-	// - - - - - - - - - -
-	/* PA1 */
-	//const data = req.nextUrl
-	//console.log(data)
-
-	/* PA2 */
 	try {
 		const form = await req.formData()
 		const params = Object.fromEntries(form)
+
+		const id = parseInt(params['data[FIELDS][ID]'])
+		const entityTypeId = parseInt(params['data[FIELDS][ENTITY_TYPE_ID]'])
 
 		switch(params.event) {
 
 			/* SPA */
 			case 'ONCRMDYNAMICITEMADD':
-					addSPA(
-						parseInt(params['data[FIELDS][ID]']),
-						parseInt(params['data[FIELDS][ENTITY_TYPE_ID]'])
-					)
+					addSPA(id, entityTypeId)
 				break
 			case 'ONCRMDYNAMICITEMUPDATE':
-					updateSPA(
-						parseInt(params['data[FIELDS][ID]']),
-						parseInt(params['data[FIELDS][ENTITY_TYPE_ID]'])
-					)
+					updateSPA(id, entityTypeId)
 				break
 			case 'ONCRMDYNAMICITEMDELETE':
-					deleteSPA(
-						parseInt(params['data[FIELDS][ID]']),
-						parseInt(params['data[FIELDS][ENTITY_TYPE_ID]'])
-					)
+					deleteSPA(id, entityTypeId)
 				break
 
 			/* LEAD */
@@ -51,19 +35,5 @@ export async function POST (req) {
 		console.log(e)
 	}
 	
-	
-	// - - - - - - - - - -
-	//socket.emit('type-of-event', 'data of event')
-
-
 	return NextResponse.json({ events:true })
 } 
-
-/* output:
-
-	{
-	  event: 'ONCRMDYNAMICITEMUPDATE',
-	  'data[FIELDS][ID]': '351',
-	  'data[FIELDS][ENTITY_TYPE_ID]': '132'
-	}
-*/
