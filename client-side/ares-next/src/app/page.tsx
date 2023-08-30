09b1684567883e3cdc6e46a017ffe78aee9d3f62
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-08-30 09:59:09 +0700

    ...

--- client-side/ares-next/src/app/page.tsx
+++ client-side/ares-next/src/app/page.tsx
@@ -41,6 +41,7 @@ export default function HomePage() {
 				<AppIconLink text="Superset" icon="/superset-icon.svg" href="http://115.75.90.30:8888" />
 				<AppIconLink text="Bitrix24" icon="/bitrix24-icon.svg" href="https://aresen.bitrix24.com" />
 				<AppIconLink text="N8N" icon="/n8n-icon.svg" href="http://115.75.90.30:5678" />
+				<AppIconLink text="Miro" icon="/miro-icon.svg" href="https://miro.com" />
 			</div>
 		</div>
 	)
