512ab7004de829e8db3d0ff0bb49e16be74796f7
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-09-25 16:30:06 +0700

    ...

--- client-side/ares-next/src/app/api/btx/crm.deal.get/route.ts
+++ client-side/ares-next/src/app/api/btx/crm.deal.get/route.ts
@@ -1,4 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server'
+import {
+	NextRequest,
+	NextResponse } from 'next/server'
 
 export async function POST (req) {
 	
--- client-side/ares-next/src/app/api/btx/crm.lead.get/route.ts
+++ client-side/ares-next/src/app/api/btx/crm.lead.get/route.ts
@@ -1,1 +1,8 @@
-//
+import {
+	NextRequest,
+	NextResponse } from 'next/server'
+import fs from 'fs'
+
+export async function POST (req) {
+	return NextResponse.json({ result:true })
+}
