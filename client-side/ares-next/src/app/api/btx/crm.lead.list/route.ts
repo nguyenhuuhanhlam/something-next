98f8980c803d5c3fbfc52f12b9f1b68b676326fc
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-10-05 15:17:12 +0700

    ...

--- client-side/ares-next/src/app/api/btx/crm.lead.list/route.ts
+++ client-side/ares-next/src/app/api/btx/crm.lead.list/route.ts
@@ -2,15 +2,31 @@ import {
 	NextRequest,
 	NextResponse } from 'next/server'
 
+import { LEAD_UFS } from '@/constants'
+
 /* - - - - - - - - - - */
 
 export async function POST (req) {
+
+	const BITRIX_ENDPOINT = process.env['NEXT_PUBLIC_BITRIX_ENDPOINT_11']
+	let all = []
+	let options = {
+		select: [
+			'ID','TITLE','STATUS_ID','SOURCE_ID','OPPORTUNITY',
+			'DATE_CREATE','DATE_CLOSED','ASSIGNED_BY_ID',
+			LEAD_UFS.BusinessSectors, LEAD_UFS.ConvertDate,
+			LEAD_UFS.FOLLOW_REASONS, LEAD_UFS.FAILED_REASONS,
+			LEAD_UFS.PROVINCE, LEAD_UFS.ACCOUNT, LEAD_UFS.SALES_TYPE, LEAD_UFS.SALES_OBJECT
+		],
+		start: 0
+	}
 	
 	const res = await fetch(
-		`${ process.env['NEXT_PUBLIC_BITRIX_ENDPOINT_11'] }/crm.lead.list`,
+		`${ BITRIX_ENDPOINT }/crm.lead.list`,
 		{
 			method:'POST',
-			headers: { 'Content-Type': 'application/json' }
+			headers: { 'Content-Type': 'application/json' },
+			body:JSON.stringify({ ...options })
 		}
 	)
 
