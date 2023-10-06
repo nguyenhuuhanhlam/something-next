b013a4c3aef09e7306ac2016b5378c8deb79da0e
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-10-06 11:20:30 +0700

    ...

--- client-side/ares-next/src/app/api/tools/sql.lead.put/route.ts
+++ client-side/ares-next/src/app/api/tools/sql.lead.put/route.ts
@@ -4,3 +4,24 @@ import {
 import excuteQuery from '@/lib/db.ts'
 
 const APP_URL = process.env.NEXT_PUBLIC_URL
+
+export async function POST (req:NextRequest) {
+	const res = await fetch(
+		`${ APP_URL }/api/btx/crm.lead.list`, {
+			method:'POST',
+			headers:{ 'Content-Type':'application/json' }
+		}
+	)
+
+	const json = await res.json()
+
+	/* SQL */
+	let sql_values = []
+	json.leads.map(v=>{
+		sql_values.push(`${v.ID}`)
+	})
+
+	console.log(sql_values)
+
+	return NextResponse.json({ overwrite: true })
+}
