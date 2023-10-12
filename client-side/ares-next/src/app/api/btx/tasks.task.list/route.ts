26d018016c7bfc7cb9d290ccd4114c4b2fc51000
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-10-12 15:04:18 +0700

    ...

--- client-side/ares-next/src/app/api/btx/tasks.task.list/route.ts
+++ client-side/ares-next/src/app/api/btx/tasks.task.list/route.ts
@@ -6,5 +6,16 @@ import {
 /* - - - - - - - - - - */
 
 export async function POST (req) {
+	
+	// 1.
+	const res = await fetch(
+		`${ BITRIX_ENDPOINT }/crm.lead.list`,
+		{
+			method:'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body:JSON.stringify({ ...options })
+		}
+	)
+
 	return NextResponse.json({ tasks:[] })
 }
