480c9f6017098e4c5efc97f9548cc09e76c21dab
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-MacBook-Air.local>
2023-11-01 20:50:13 +0700

    ...

--- server-side/strapi-ahri/src/api/project-timeline/routes/project-timeline.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-timeline router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::project-timeline.project-timeline');
--- server-side/strapi-ahri/src/api/project-timeline/services/project-timeline.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-timeline service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::project-timeline.project-timeline');
