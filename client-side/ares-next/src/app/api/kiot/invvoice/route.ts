a7fd5b537ec0357b2203364bc2f4807764100587
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2023-11-02 22:44:29 +0700

    ...

---
+++ client-side/ares-next/src/app/api/kiot/invvoice/route.ts
@@ -0,0 +1,131 @@
+/**
+ * UTILS
+ */
+function customerFFilter(item) {
+  let reItem = {}
+  Object.keys(item).map(k=>{
+    reItem['code'] = item['code']
+    reItem['name'] = item['name']
+    reItem['birthDate'] = item['birthDate']||null
+    reItem['contactNumber'] = item['contactNumber']
+    reItem['totalInvoiced'] = item['totalInvoiced']
+  })
+  return reItem 
+}
+function invoiceFFilter(item) {
+  let reItem = {}
+  Object.keys(item).map(k=>{
+    reItem['code'] = item['code']
+    reItem['branchId'] = item['branchId']
+    reItem['branchName'] = item['branchName']
+    reItem['soldById'] = item['soldById']
+    reItem['soldByName'] = item['soldByName']
+    reItem['customerCode'] = item['customerCode']
+    reItem['customerName'] = item['customerName']
+    reItem['purchaseDate'] = item['purchaseDate']
+    reItem['total'] = item['total']
+    reItem['status'] = item['status']
+    reItem['statusValue'] = item['statusValue']
+  })
+  return reItem
+}
+/**
+ * REQ FUNCTIONS
+ */
+function KiotVietGetToken() {
+  let o = {
+    method:'post',
+    contentType:'application/x-www-form-urlencoded',
+    payload:{
+      client_id:'e79b9cba-ea82-46c8-987b-7fb1facb03ca',
+      client_secret:'F52A4DC060F1E358BA773FECFC02F86362864804',
+      grant_type:'client_credentials',
+      scopes:'PublicApi.Access'
+    }
+  }
+  let r = UrlFetchApp.fetch('https://id.kiotviet.vn/connect/token', o)
+  return JSON.parse(r.getContentText()).access_token
+}
+/**
+ * 
+ */
+function FetchCustomers() {  
+  /**
+   * 1.
+   */
+  let o = {
+    method:'get',
+    headers: {
+      Retailer: 'nhathuockanen',
+      Authorization: 'Bearer ' + KiotVietGetToken()
+    }
+  }
+  /**
+   * 2.
+   */
+  let result = []
+  let pageSize = 100 
+  let r = UrlFetchApp.fetch(`https://public.kiotapi.com/customers?includeTotal=true&pageSize=${pageSize}&currentItem=0`, o)
+  let json = JSON.parse(r.getContentText())
+  /**
+   *  3.
+   */
+  let filteringData = json.data.map(v=>customerFFilter(v))
+  let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Date-Customer')
+  sheet.clearContents()
+  sheet.getRange(1,1,1,Object.keys(filteringData[0]).length).setValues([Object.keys(filteringData[0])])
+  /**
+   * 4.
+   */
+  result = filteringData.map(v=>Object.values(v))
+  /** */
+
+  for(i=1; i<=parseInt(json.total/pageSize); i++) {
+    let r = UrlFetchApp.fetch(`https://public.kiotapi.com/customers?includeTotal=true&pageSize=${pageSize}&currentItem=${i*pageSize}`, o)
+    let json = JSON.parse(r.getContentText())
+    let filteringData = json.data.map(v=>customerFFilter(v))
+
+    for (let j=0; j<filteringData.length; j++) {
+      let item = filteringData[j]
+      result.push(Object.values(item))
+    }
+  }
+
+  sheet.getRange(2, 1,result.length,Object.keys(filteringData[0]).length).setValues(result)
+}
+/**
+ * 
+ */
+function FetchInvoices() {
+  let options = {method:'get',headers:{Retailer:'nhathuockanen',Authorization:'Bearer '+KiotVietGetToken()}}
+
+  let result = []
+  let pageSize = 100 
+
+  let r = UrlFetchApp.fetch(`https://public.kiotapi.com/invoices?pageSize=${pageSize}`,options)
+  let json = JSON.parse(r.getContentText())
+
+  //console.log(json)
+  //return
+
+  let filteringData = json.data.map(v=>invoiceFFilter(v))
+
+  let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Date-Invoice')
+  sheet.clearContents()
+  sheet.getRange(1,1,1,Object.keys(filteringData[0]).length).setValues([Object.keys(filteringData[0])])
+  
+  result = filteringData.map(v=>Object.values(v))
+
+  for(i=1; i<=parseInt(json.total/pageSize); i++) {
+    let r = UrlFetchApp.fetch(`https://public.kiotapi.com/invoices?pageSize=${pageSize}&currentItem=${i*pageSize}`,options)
+    let json = JSON.parse(r.getContentText())
+    let filteringData = json.data.map(v=>invoiceFFilter(v))
+
+    for (let j=0; j<filteringData.length; j++) {
+      let item = filteringData[j]
+      result.push(Object.values(item))
+    }
+  }
+
+  sheet.getRange(2, 1,result.length,Object.keys(filteringData[0]).length).setValues(result)
+}
--- client-side/ares-next/src/app/page.tsx
+++ client-side/ares-next/src/app/page.tsx
@@ -35,15 +35,17 @@ export default function HomePage() {
 	return (
 		<div className="home">
 			<div className="app-icons my-5">
-				<AppIconLink text="E-Book" icon="/ebook-icon.svg" href="http://ebook.aresen.vn:8088" />
 				<AppIconLink text="NAS Drive" icon="/nas-drive-icon.svg" href="http://drive.aresen.vn" />
 				<AppIconLink text="NAS Cloud" icon="/nas-synology-icon.svg" href="http://cloud.aresen.vn" />
 				<AppIconLink text="NAS Photo" icon="/nas-photo-icon.svg" href="http://115.75.90.30:5000/?launchApp=SYNO.Foto.AppInstance&SynoToken=QOfnfUXTMizc2#/shared_space/folder/1" />
-				<AppIconLink text="Superset" icon="/superset-icon.svg" href="http://115.75.90.30:8888" />
+			</div>
+			<div className="app-icons my-5">
 				<AppIconLink text="Bitrix24" icon="/bitrix24-icon.svg" href="https://aresen.bitrix24.com" />
 				<AppIconLink text="Miro" icon="/miro-icon.svg" href="https://miro.com" />
+				<AppIconLink text="E-Book" icon="/ebook-icon.svg" href="http://ebook.aresen.vn:8088" />
 			</div>
-			<div className="app-icons my-5">				
+			<div className="app-icons my-5">
+				<AppIconLink text="Superset" icon="/superset-icon.svg" href="http://115.75.90.30:8888" />				
 				<AppIconLink text="N8N" icon="/n8n-icon.svg" href="http://115.75.90.30:5678" />
 			</div>
 		</div>
