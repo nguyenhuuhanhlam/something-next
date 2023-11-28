cd897f06e197df4d4b388a89030976a0fef1173f
DESKTOP-31CNHHG\NHHL <nguyenhuuhanhlam@gmail.com>
2023-11-28 11:28:09 +0700

    no message

--- client-side/ares-next/src/app/api/events/spa.rebuilds.ts
+++ client-side/ares-next/src/app/api/events/spa.rebuilds.ts
@@ -9,21 +9,18 @@ export const rebuilds = (entityTypeId, item=null) =>
 			break
 		case 131:
 			b = {
-					spaType:131,
-					data: {
-						Id: item.id,
-						Title: item.title,
-						CompanyID: item.companyId,
-						Stage: item.stageId,
-						Responsible: item.assignedById,
-						CreatedDate: item.createdTime,
-						MovedDate: item.movedTime,
-						NgayTapKet: item.ufCrm15_1649732702,
-						NgayLapDatHoanThanh: item.ufCrm15_1649732751,
-						NgayThuMauNoiBo: item.ufCrm15_1649732783,
-						NgayThuMauAB: item.ufCrm15_1649732806,
-						NgayBanGiao: item.ufCrm15_1649732833
-					}
+				Id: item.id,
+				Title: item.title,
+				CompanyID: item.companyId,
+				Stage: item.stageId,
+				Responsible: item.assignedById,
+				CreatedDate: item.createdTime,
+				MovedDate: item.movedTime,
+				NgayTapKet: item.ufCrm15_1649732702,
+				NgayLapDatHoanThanh: item.ufCrm15_1649732751,
+				NgayThuMauNoiBo: item.ufCrm15_1649732783,
+				NgayThuMauAB: item.ufCrm15_1649732806,
+				NgayBanGiao: item.ufCrm15_1649732833
 			}
 			break
 	}
