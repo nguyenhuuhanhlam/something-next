2a7386a94a4c884dc6e5023f8f457ac5afe98e84
DESKTOP-31CNHHG\NHHL <nguyenhuuhanhlam@gmail.com>
2023-11-28 11:23:23 +0700

    no message

--- client-side/ares-next/src/app/api/events/spa.rebuilds.ts
+++ client-side/ares-next/src/app/api/events/spa.rebuilds.ts
@@ -16,6 +16,13 @@ export const rebuilds = (entityTypeId, item=null) =>
 						CompanyID: item.companyId,
 						Stage: item.stageId,
 						Responsible: item.assignedById,
+						CreatedDate: item.createdTime,
+						MovedDate: item.movedTime,
+						NgayTapKet: item.ufCrm15_1649732702,
+						NgayLapDatHoanThanh: item.ufCrm15_1649732751,
+						NgayThuMauNoiBo: item.ufCrm15_1649732783,
+						NgayThuMauAB: item.ufCrm15_1649732806,
+						NgayBanGiao: item.ufCrm15_1649732833
 					}
 			}
 			break
