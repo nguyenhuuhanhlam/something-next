a0340780948a8cd16f1710f0ec3a75baff2317e8
DESKTOP-31CNHHG\NHHL <nguyenhuuhanhlam@gmail.com>
2023-12-01 17:05:27 +0700

    ...

--- client-side/ares-next/src/app/api/btx/oauth/route.ts
+++ client-side/ares-next/src/app/api/btx/oauth/route.ts
@@ -4,5 +4,8 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST (req)
 {
+	const body = await req.json()
+	console.log(body)
+
 	return NextResponse.json({ hello: "all" })
 }
