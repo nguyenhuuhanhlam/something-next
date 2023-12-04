b6d1d2712ca67901d9952ba5e7a0662a50ed1253
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-12-04 21:57:20 +0700

    ...

--- client-side/pannellum-next/_.js
+++ client-side/pannellum-next/_.js
@@ -18,7 +18,6 @@ const removeInfo = () => {}
 var scenes =
 {
 	F0_1: {
-		title:'LỐI VÀO CHÍNH',
 		type:'equirectangular',
 		panorama:'uploads/VR360/F0_LOIVAOCHINH/F0_1.jpg',
 		hotSpots: [
@@ -26,9 +25,12 @@ var scenes =
 		]
 	},
 	F0_2: {
-		title:'CỬA CHÍNH',
 		type:'equirectangular',
 		panorama:'uploads/VR360/F0_LOIVAOCHINH/F0_2.jpg',
+		hotSpots: [
+			{ pitch:-40, yaw:2, type:'scene', sceneId:'F0_1' },
+			{ pitch:-10, yaw:2, type:'scene', sceneId:'F1_1', text:'VÀO SẢNH CHÍNH' },
+		]
 		// hotSpots: [
 		// 	{ pitch:45, yaw:0, type:'scene', text:'To A', sceneId:'S3' },
 		// 	{
@@ -51,20 +53,19 @@ var scenes =
 		// 	}
 		// ]
 	},
-	S3: {
-		title:'Library',
+	F1_1: {
 		type: 'equirectangular',
-		panorama: 'uploads/library.jpg',
-		hotSpots: [
-			{ pitch:0, yaw:0, type:'scene', text:'To B1', sceneId:'S0' }
-		]
+		panorama: 'uploads/VR360/F1_PANEL/F1_1.jpg',
+		// hotSpots: [
+		// 	{ pitch:0, yaw:0, type:'scene', text:'To B1', sceneId:'S0' }
+		// ]
 	}
 }
 
 var start =
 {
 	default: {
-		firstScene: 'F0_1',
+		firstScene: 'F0_2',
 		sceneFadeDuration: 2000,
 		autoLoad: true,
 	},
