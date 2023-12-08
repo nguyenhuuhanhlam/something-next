d77eb0cc9a828d0889fbe08e8f9f067c04066722
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-12-08 23:33:08 +0700

    ...

--- client-side/ares-next/src/app/api/tools/sql.items_136_51.put/route.ts
+++ client-side/ares-next/src/app/api/tools/sql.items_136_51.put/route.ts
@@ -1,1 +1,61 @@
-//
+import { NextRequest, NextResponse } from 'next/server'
+import excuteQuery from '@/lib/db'
+import { ParseString as ps, ParseDateString as pds } from '@/lib/db.helpers'
+import { SPA_UFS } from '@/constants'
+
+export async function POST (req:NextRequest)
+{
+	const table = 'spa_136_51'
+	const res = await fetch(
+		`${ process.env.NEXT_PUBLIC_URL }/api/btx/crm.item_136_51.list`, {
+			method:'POST',
+			headers:{ 'Content-Type':'application/json' }
+		}
+	)
+
+	const json = await res.json()
+
+	/* SQL */
+	let sql_values = []
+	json.spas.map(v => {
+		const item = {
+			Id: v.id,
+			Title: ps(v.title),
+			CreatedDate: pds(v.createdTime),
+			MovedDate: pds(v.movedTime),
+			Stage: ps(v.stageId),
+			CompanyID: v.companyId || 'NULL',
+			Responsible: v.assignedById || 'NULL',
+
+			NgayThuThapDuTaiLieu: pds(v[SPA_UFS[136]['NgayThuThapDuTaiLieu']]),
+			NgayHoanThanhBaiViet: pds(v[SPA_UFS[136]['NgayHoanThanhBaiViet']]),
+			NgayNopThamDinh: pds(v[SPA_UFS[136]['NgayNopThamDinh']]),
+			NgayThamDinh: pds(v[SPA_UFS[136]['NgayThamDinh']]),
+			NgayNopChinhSua: pds(v[SPA_UFS[136]['NgayNopChinhSua']]),
+			NgayNhanQuyetDinh: pds(v[SPA_UFS[136]['NgayNhanQuyetDinh']]),
+			NgayNghiemThuBanGiao: pds(v[SPA_UFS[136]['NgayNghiemThuBanGiao']]),
+			HienTrang: ps(v[SPA_UFS[136]['HienTrang']]),
+			KhoKhan: ps(v[SPA_UFS[136]['KhoKhan']]),
+			GiaiPhap: ps(v[SPA_UFS[136]['GiaiPhap']]),
+		}
+
+		sql_values.push(`(${ Object.keys(item).map(k=>item[k]) })`)
+	})
+
+	const q = `INSERT INTO ${table}
+				(Id,Title,CreatedDate,MovedDate,Stage,CompanyID,Responsible,
+				NgayThuThapDuTaiLieu,NgayHoanThanhBaiViet,NgayNopThamDinh,
+				NgayThamDinh,NgayNopChinhSua,NgayNhanQuyetDinh,NgayNghiemThuBanGiao,HienTrang,KhoKhan,GiaiPhap)
+			VALUES ${sql_values.join()}`
+
+	try {
+		const del_result = await excuteQuery({ query: `DELETE FROM ${table}` })
+		const ins_result = await excuteQuery({
+			query: q.replace(/(\r\n|\n|\r|\t)/gm,'')
+		})
+	} catch (e) {
+		console.log(e)
+	}
+
+	return NextResponse.json({ overwrite: true })
+}
