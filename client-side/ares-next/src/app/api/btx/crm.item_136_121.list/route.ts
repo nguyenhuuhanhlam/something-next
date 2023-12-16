4c15e7a7d7257f781d9185e654e60b900e8b446e
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2023-12-16 21:00:39 +0700

    ...

--- client-side/ares-next/src/app/api/btx/crm.item_136_121.list/route.ts
+++ client-side/ares-next/src/app/api/btx/crm.item_136_121.list/route.ts
@@ -28,17 +28,6 @@ export async function POST (req)
 			SPA_UFS[136]['TyLePhaSinhGiaGoc'],
 			SPA_UFS[136]['BienPhiDuAn'],
 			SPA_UFS[136]['E01QuanLyBaiViet']
-
-	// 		SPA_UFS[136]['NgayThuThapDuTaiLieu'],
-	// 		SPA_UFS[136]['NgayHoanThanhBaiViet'],
-	// 		SPA_UFS[136]['NgayNopThamDinh'],
-	// 		SPA_UFS[136]['NgayThamDinh'],
-	// 		SPA_UFS[136]['NgayNopChinhSua'],
-	// 		SPA_UFS[136]['NgayNhanQuyetDinh'],
-	// 		SPA_UFS[136]['NgayNghiemThuBanGiao'],
-	// 		SPA_UFS[136]['HienTrang'],
-	// 		SPA_UFS[136]['KhoKhan'],
-	// 		SPA_UFS[136]['GiaiPhap']
 		]
 	}
 
@@ -57,25 +46,25 @@ export async function POST (req)
 
 	all = all.concat(items)
 
-	// // REPEAT //
+	// REPEAT //
 
-	// const page_size = 50
-	// if (next) {
-	// 	for (let i=1; i<=Math.floor(total/page_size); i++) {
-	// 		const res = await fetch(
-	// 			`${ endpoint }/crm.item.list`,
-	// 			{
-	// 				method:'POST',
-	// 				headers: { 'Content-Type': 'application/json' },
-	// 				body:JSON.stringify({ ...body, ...options, start: i*page_size })
-	// 			}
-	// 		)
+	const page_size = 50
+	if (next) {
+		for (let i=1; i<=Math.floor(total/page_size); i++) {
+			const res = await fetch(
+				`${ endpoint }/crm.item.list`,
+				{
+					method:'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body:JSON.stringify({ ...body, ...options, start: i*page_size })
+				}
+			)
 
-	// 		const { result:{items} } = await res.json()
+			const { result:{items} } = await res.json()
 
-	// 		all = all.concat(items)
-	// 	}
-	// }
+			all = all.concat(items)
+		}
+	}
 
 	return NextResponse.json({ spas: all })
 }
---
+++ client-side/ares-next/src/app/api/tools/sql.items_136_121.put/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from 'next/server'
+import excuteQuery from '@/lib/db'
+import { ParseString as ps, ParseDateString as pds } from '@/lib/db.helpers'
+import { SPA_UFS } from '@/constants'
+
+export async function POST (req:NextRequest)
+{
+	const table = 'spa_136_121'
+	const res = await fetch(
+		`${ process.env.NEXT_PUBLIC_URL }/api/btx/crm.item_136_121.list`, {
+			method:'POST',
+			headers:{ 'Content-Type':'application/json' }
+		}
+	)
+
+	const json = await res.json()
+
+	/* SQL */
+	let sql_values = []
+
+	// json.spas.map(v => {
+	// 	const item = {
+	// 		Id: v.id,
+	// 		Title: ps(v.title),
+	// 		CreatedDate: pds(v.createdTime),
+	// 		MovedDate: pds(v.movedTime),
+	// 		Stage: ps(v.stageId),
+	// 		CompanyID: v.companyId || 'NULL',
+	// 		Responsible: v.assignedById || 'NULL',
+	// 		NgayTapKet: pds(v[SPA_UFS[131]['NgayTapKet']]),
+	// 		NgayLapDatHoanThanh: pds(v[SPA_UFS[131]['NgayLapDatHoanThanh']]),
+	// 		NgayThuMauNoiBo: pds(v[SPA_UFS[131]['NgayThuMauNoiBo']]),
+	// 		NgayThuMauAB: pds(v[SPA_UFS[131]['NgayThuMauAB']]),
+	// 		NgayBanGiao: pds(v[SPA_UFS[131]['NgayBanGiao']]),
+	// 		HienTrang: ps(v[SPA_UFS[131]['HienTrang']]),
+	// 		KhoKhan: ps(v[SPA_UFS[131]['KhoKhan']]),
+	// 		GiaiPhap: ps(v[SPA_UFS[131]['GiaiPhap']]),
+	// 	}
+
+	// 	sql_values.push(`(${ Object.keys(item).map(k=>item[k]) })`)
+	// })
+
+	// const q = `INSERT INTO ${table}
+	// 		(Id,Title,CreatedDate,MovedDate,Stage,CompanyID,Responsible,
+	// 		NgayTapKet,NgayLapDatHoanThanh,NgayThuMauNoiBo,NgayThuMauAB,NgayBanGiao,HienTrang,KhoKhan,GiaiPhap)
+	// 	VALUES ${sql_values.join()}`
+
+	// try {
+	// 	const del_result = await excuteQuery({ query: `DELETE FROM ${table}` })
+	// 	const ins_result = await excuteQuery({
+	// 		query: q.replace(/(\r\n|\n|\r|\t)/gm,'')
+	// 	})
+	// } catch (e) {
+	// 	console.log(e)
+	// }
+
+	return NextResponse.json({ overwrite: true })
+}
