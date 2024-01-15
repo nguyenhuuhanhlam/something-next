49f32f6b87adb292050f4fb312284b8007071ae8
DESKTOP-PTGLIQA\NHHL <nguyenhuuhanhlam@gmail.com>
2024-01-15 11:13:09 +0700

    ...

---
+++ client-side/ares-next/src/app/api/kiot/invoice.range.list/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest,NextResponse } from 'next/server'
+import _ from 'lodash'
+
+export async function POST (req)
+{
+	const res = await fetch(`${ process.env.NEXT_PUBLIC_URL }/api/kiot/token.get`,{ method:'POST' })
+	const { access_token } = await res.json()
+
+	const pageSize = 100
+	const options = {
+		method:'GET',
+		headers: {
+			'Content-Type':'application/json',
+			Retailer:'nhathuockanen',
+			Authorization:'Bearer ' + access_token
+		},
+	}
+	const picks = ['code','branchId','branchName','soldById','soldByName','customerCode','customerName','purchaseDate','total','statusValue']
+
+	const response = await fetch(`https://public.kiotapi.com/invoices?pageSize=${pageSize}`, options)
+	const { data,total } = await response.json()
+
+	// 1.
+	let all = []
+	data.map(v=>{ all.push(_.pick(v, picks)) })
+
+	// 2.
+	if (total>pageSize)
+	{
+		for(let i=1; i<=total/pageSize; i++)
+		{
+			const response = await fetch(`https://public.kiotapi.com/invoices?pageSize=${pageSize}&currentItem=${i*pageSize}`, options)
+			const { data } = await response.json()
+
+			data.map(v=>{ all.push(_.pick(v, picks)) })
+		}
+	}
+
+	return NextResponse.json({ invoices:all })
+}
