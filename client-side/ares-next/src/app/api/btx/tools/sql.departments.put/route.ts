a6f5967a4a22bbbd8c9576d890a8d02dabc37002
DESKTOP-PTGLIQA\NHHL <nguyenhuuhanhlam@gmail.com>
2024-01-18 09:18:27 +0700

    just...

--- client-side/ares-next/src/app/api/btx/tools/sql.departments.put/route.ts
+++ client-side/ares-next/src/app/api/btx/tools/sql.departments.put/route.ts
@@ -1,1 +1,16 @@
-//
+import {
+	NextRequest,
+	NextResponse } from 'next/server'
+import excuteQuery from '@/lib/db.ts'
+
+export async function POST (req:NextRequest)
+{
+	const res = await fetch(
+		`${ process.env.NEXT_PUBLIC_URL }/api/btx/department.get`, {
+			method:'POST',
+			headers:{ 'Content-Type':'application/json' }
+		}
+	)
+
+	return NextResponse.json({ overwrite: true })
+}
--- client-side/ares-next/src/app/api/btx/tools/sql.user.put/route.ts
+++ client-side/ares-next/src/app/api/btx/tools/sql.user.put/route.ts
@@ -3,12 +3,10 @@ import {
 	NextResponse } from 'next/server'
 import excuteQuery from '@/lib/db.ts'
 
-const APP_URL = process.env.NEXT_PUBLIC_URL
-
 export async function POST (req:NextRequest) {
 
 	const res = await fetch(
-		`${ APP_URL }/api/btx/user.get`, {
+		`${ process.env.NEXT_PUBLIC_URL }/api/btx/user.get`, {
 			method:'POST',
 			headers:{ 'Content-Type':'application/json' }
 		}
